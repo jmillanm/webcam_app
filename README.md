c8d9458c11ec85a69428971346bbb178185fc3b5
jhanett.millan <jhanett.millan@almundo.com>
2020-05-12 09:19:03 -0300

    webcam app

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# test_flutter
-testing flutter app dev
+# Test: webcam app
--- candle_chart_js/Scripts/config.js
+++
@@ -1,8 +0,0 @@
-﻿var require = {
-	baseUrl: "",
-	paths: {
-		'jquery': './Scripts/jquery',
-		'MindFusion.Common': './Scripts/MindFusion.Common',
-		'MindFusion.Gauges': './Scripts/MindFusion.Gauges',
-	},
-};
